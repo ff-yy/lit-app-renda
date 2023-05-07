c21eef96eb69b468906d95a83a0de42c4016e85c
ff-yy <yuya12121@gmail.com>
2023-05-07 17:27:01 +0900

    アプリ実装

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet var tapButton: UIButton!
+    
+    var tapCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        tapButton.layer.cornerRadius = 250
+    }
+    
+    @IBAction func tapTapButton() {
+        tapCount += 1
+        countLabel.text = String(tapCount)
+        
+        if tapCount >= 10 {
+            countLabel.textColor = UIColor.red
+        }
+        else {
+            countLabel.textColor = UIColor.black
+            
+        }
     }
 
 
